9527c4c16073cb315d3d2bb3bedca6107f4ea985
fangcanlin <fcanlin@126.com>
2016-10-31 00:04:43 +0800

    code
    mysqli_query($sqldb, "set names utf8");
    使用这句来设置数据库乱码问题。。。

---
+++ project1/login.php
@@ -0,0 +1,52 @@
+﻿<?php
+session_start();
+header("Content-type:text/html; charset=utf-8"); 
+
+if (empty($_POST))
+{
+	echo "error!!!";
+	exit;
+}
+$phone = "";
+$password = "";
+if (isset($_POST["account"]))
+{
+	$phone = $_POST["account"];
+}
+else
+{
+	echo "not account!!!";
+	exit();
+}
+if (isset($_POST["password"]))
+{
+	$password = $_POST["password"];
+}
+else
+{
+	echo "not password!!!";
+	exit();
+}
+$sqldb = new mysqli("localhost", "root", "", "project");
+if (mysqli_connect_errno())
+{
+	printf("Connect failed:%s\n", mysqli_connect_error());
+	exit();
+}
+mysqli_query($sqldb, "set names utf8");
+$sql = "select count(*) from accounts  where `userPhone` = '".$phone."' and `password` = '".$password."'";
+$result = mysqli_query($sqldb, $sql);
+$row = mysqli_fetch_row($result);
+mysqli_free_result($result);
+mysqli_close($sqldb);
+if ($row[0] == 1)
+{
+	$_SESSION["phone"] = $phone;
+	header('Location:index.html');
+	exit();
+}
+else
+{
+	echo "no exit";
+}
+?>
---
+++ project1/writeRecommendInfo.php
@@ -0,0 +1,18 @@
+﻿<?php
+session_start();
+header("Content-type:text/html; charset=utf-8"); 
+$phoneRecommend = $_SESSION["phone"];
+$reName = $_POST["reName"];
+$rePhone = $_POST["rePhone"];
+$reInfo = $_POST["reInfo"];
+$sqldb = new mysqli("localhost", "root", "", "project");
+if (mysqli_connect_errno())
+{
+	printf("Connect failed:%s\n", mysqli_connect_error());
+	exit();
+}
+mysqli_query($sqldb, "set names utf8");
+$sql = "INSERT INTO recommend(`phoneRecommend`, `recommendPhone`, `recommendName`, `recommendInfo`, `recommendStatus`, `createDate`, `updateDate`) values('".$phoneRecommend."','".$rePhone."','".$reName."','".$reInfo."',0,now(),now())";
+mysqli_query($sqldb, $sql);
+mysqli_close($sqldb);
+?>
